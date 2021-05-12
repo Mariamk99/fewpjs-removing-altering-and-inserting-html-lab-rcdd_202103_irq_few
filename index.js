f9459981f0dd02d30952a93e24dc3af081091bfb
Mariam  Khamo <mariamw736@gmail.com>
2021-05-12 20:48:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,17 @@
 // Update elements using innerHTML
 // Change properties on DOM nodes
 // Remove elements from the DOM
+<<<<<<< HEAD
 document.getElementById('main').remove();
 
 const newHeader = document.createElement('h1')
 newHeader.id = 'victory'
 newHeader.innerHTML = "YOUR-NAME is the champion"
+=======
+let main = document.getElementById("main");
+let element = document.createElement("div");
+element.appendchild("main");
+element.setAttribute("id", "victory");
+let header = document.getElementById("h1#victory").appendchild("div");
+header.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
+>>>>>>> 2fa7bb9685aa03caac2d88fbfd2d757749e4dd6e
